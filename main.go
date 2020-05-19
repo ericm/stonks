aa0aef6a94adc0ec4a688cc90d33d260903c3d73
Eric Moynihan <git@moynihan.io>
2020-05-19 10:29:40 +0100

    Adding favourites

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 	"strings"
 	"time"
 
@@ -15,24 +16,62 @@ import (
 
 var (
 	interval *string
+	save     *string
+	name     *string
 	week     *bool
 	days     *int
+
+	configPath string
 )
 
-func main() {
+func setDefaults() {
 	home, ok := os.LookupEnv("HOME")
 	if !ok {
 		panic("No HOME env var set")
 	}
-	viper.AddConfigPath(fmt.Sprintf("%s/.config/stonks/", home))
-	viper.SetConfigName("favourites")
+	configPath = fmt.Sprintf("%s/.config", home)
+	viper.AddConfigPath(configPath)
+	viper.SetConfigName("stonks")
 	viper.SetConfigType("yaml")
+
+	viper.SetDefault("favourites", map[string]string{})
+
 	viper.ReadInConfig()
+}
+
+func main() {
+	setDefaults()
 	rootCmd := &cobra.Command{
 		Use:   "stonks",
 		Short: "A stock visualizer",
 		Long:  "Displays realtime stocks in graph format in a terminal",
 		Run: func(cmd *cobra.Command, args []string) {
+			if len(*save) > 0 {
+				*save = strings.ToUpper(*save)
+				if _, err := api.GetChart(strings.ToUpper(*save), datetime.FifteenMins, nil, nil); err != nil {
+					fmt.Println(err.Error())
+					os.Exit(1)
+				}
+				favourites, ok := viper.Get("favourites").(map[string]string)
+				if !ok {
+					fmt.Println("Read config error")
+					os.Exit(1)
+				}
+				nameCmd := *save
+				if len(*name) > 0 {
+					nameCmd = *name
+				}
+				favourites[*save] = nameCmd
+				viper.Set("favourites", favourites)
+				if err := viper.WriteConfig(); err != nil {
+					err = viper.WriteConfigAs(path.Join(configPath, "stonks"))
+					if err != nil {
+						fmt.Println(err.Error())
+						os.Exit(1)
+					}
+				}
+				return
+			}
 			for _, symbol := range args {
 				var intervalCmd datetime.Interval
 				var start *datetime.Datetime
@@ -70,5 +109,8 @@ func main() {
 	interval = rootCmd.PersistentFlags().StringP("interval", "i", "15m", "stonks -t X[m|h] (eg 15m, 5m, 1h, 1d)")
 	week = rootCmd.PersistentFlags().BoolP("week", "w", false, "Display the last week (will set interval to 1d)")
 	days = rootCmd.PersistentFlags().IntP("days", "d", 0, "Stocks from X number of days ago.")
+	save = rootCmd.PersistentFlags().StringP("save", "s", "", "Add an item to the default stonks command. (Eg: -s AMD -n \"Advanced Micro Devices\")")
+	name = rootCmd.PersistentFlags().StringP("name", "n", "", "Optional name for a stonk save")
+
 	rootCmd.Execute()
 }
